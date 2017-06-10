c01ea14e390116c79fe821bd31ba07ee9cb2d4ca
Texdarkstar <texdarkstar@fastmail.fm>
2017-06-09 21:48:57 -0500

    added install instructions for mushclient plugins

--- src/web/urls.py
+++ src/web/urls.py
@@ -17,7 +17,9 @@ custom_patterns = [
     url(r'^plugins/', include('web.plugin_index.urls', namespace='plugin_index', app_name='plugin_index')),
 
 # resources
-    url(r'^plugins/GameServer\_command\_plugin\.zip$',  RedirectView.as_view(url='/media/download/GameServer_command_plugin.zip', permanent=False))
+    url(r'^plugins/GameServer\_command\_plugin\.zip$',  RedirectView.as_view(url='/media/download/GameServer_command_plugin.zip', permanent=False)),
+    url(r'^plugins/plugin\_install\_ss1\.png$',  RedirectView.as_view(url='/media/plugin_install_ss1.png', permanent=False)),
+    url(r'^plugins/plugin\_install\_ss2\.png$',  RedirectView.as_view(url='/media/plugin_install_ss2.png', permanent=False)),
 ]
 
 # this is required by Django.
