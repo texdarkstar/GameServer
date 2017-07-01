9c94fc748706aab40c70fd82ce8e17443199de4c
Texdarkstar <texdarkstar@fastmail.fm>
2017-07-01 18:55:24 -0500

    cleaned up cmdperform a ton

--- src/commands/cmdperform.py
+++ src/commands/cmdperform.py
@@ -24,7 +24,6 @@ class CmdPerform(Command):
 
     def func(self):
         args = self.args.strip().lower()
-        result = 0
         attrdm = 0
         skillrank = 0
         dice = []
@@ -33,9 +32,12 @@ class CmdPerform(Command):
         pm2 = ""
         string = ""
 
+        regexp1 = r"^(\w+)$"
+        regexp2 = r"^(.+) +?\+ +?(\w+)$"
 
-        if re.match(r"^(\w+)$", args):  # checking for something like 'perform str'
-            m = re.match(r"^(\w+)$", args)
+
+        if re.match(regexp1, args):  # checking for something like 'perform str'
+            m = re.match(regexp1, args)
             string = "You rolled a |c{result}|n: dice rolled[|c{die1}|n, |c{die2}|n] {pm}{attr}[|c{attrdm}|n]"
 
             if args not in ['str', 'dex', 'end', 'int', 'edu', 'soc']:
@@ -58,8 +60,9 @@ class CmdPerform(Command):
             self.caller.msg(string)
             return
 
-        elif re.match(r"^(\w+) +?\+ +?(\w+)$", args):  # checking for something like 'perform broker + int'
-            m = re.match(r"^(\w+) +?\+ +?(\w+)$", args)
+
+        elif re.match(regexp2, args):  # checking for something like 'perform broker + int', or 'perform pilot(spacecraft) + dex'
+            m = re.match(regexp2, args)
             string = "You rolled a |c{result}|n: dice rolled[|c{die1}|n, |c{die2}|n] {pm1}{skillname}[|c{skilldm}|n] {pm2}{attr}[|c{attrdm}|n]"
             skillname, attr = m.groups()
 
@@ -77,6 +80,7 @@ class CmdPerform(Command):
 
             if len(skills) == 0:
                 skillrank = -3
+                skill = skillname + "[|runskilled|n]"
 
             elif len(skills) > 1:
                 string = "There were multiple matches for '%s': %s." % (skillname, ', '.join(skills))
@@ -85,68 +89,8 @@ class CmdPerform(Command):
 
             elif len(skills) == 1:
                 skill = skills.pop()
+                skillrank = self.caller.db.skills[skill]
 
-            skillrank = self.caller.db.skills[skill]
-
-            attrdm = dm_attr(self.caller.db.attrs[attr]['current'])
-
-
-            if attrdm >= 0:
-                pm2 = "+ "
-            elif attrdm < 0:
-                pm2 = "- "
-
-            if skillrank >= 0:
-                pm1 = "+ "
-            elif skillrank < 0:
-                pm1 = "- "
-
-
-            string = string.format(
-                result=dice[0] + dice[1] + attrdm + skillrank,
-                die1=dice[0],
-                die2=dice[1],
-                pm1=pm1,
-                pm2=pm2,
-                attr=attr,
-                attrdm=attrdm,
-                skillname=skill,
-                skilldm=skillrank,
-                )
-
-            self.caller.msg(string)
-            return
-
-
-        elif re.match(r"^(.+\(.+)\) +?\+ +?(\w+)$", args):  # checking for something like 'perform pilot(spacecraft) + dex'
-            m = re.match(r"^(.+\(.+)\) +?\+ +?(\w+)$", args)
-            string = "You rolled a |c{result}|n: dice rolled[|c{die1}|n, |c{die2}|n] {pm1}{skillname}[|c{skilldm}|n] {pm2}{attr}[|c{attrdm}|n]"
-            skillname, attr = m.groups()
-
-            if attr not in ['str', 'dex', 'end', 'int', 'edu', 'soc']:
-                self.caller.msg("Attribute is invalid. Select from: str, dex, end, int, edu, soc.")
-                return
-
-            dice = [die(1, 6), die(1, 6)]
-
-            skills = []
-
-            for skill in self.caller.db.skills.keys():
-                if skillname == skill[:len(skillname)]:
-                    skills.append(skill)
-
-            if len(skills) == 0:
-                skillrank = -3
-
-            elif len(skills) > 1:
-                string = "There were multiple matches for '%s': %s." % (skillname, ', '.join(skills))
-                self.caller.msg(string)
-                return
-
-            elif len(skills) == 1:
-                skill = skills.pop()
-
-            skillrank = self.caller.db.skills[skill]
 
             attrdm = dm_attr(self.caller.db.attrs[attr]['current'])
 
